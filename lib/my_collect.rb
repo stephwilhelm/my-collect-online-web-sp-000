04038b0e2ca2ba1511bf2cfa7261de4817fbe161
Steph W <wilhelm.steph@gmail.com>
2020-04-08 18:03:16 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,8 @@
-def my_collect
+def my_collect(languages, students)
+  i = 0
+  while i <languages.length
+  yield(array[i])
+  i = i + 1
   my_collect(languages) do |language|
     language.upcase
   end
